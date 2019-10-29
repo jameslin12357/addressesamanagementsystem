8ed542743a13bd246d4bd6cafbddd025784e80cd
jameslin12357 <jameslin12357@qq.com>
2019-10-29 15:51:29 +0800

    cm1

---
+++ src/main/java/com/example/addressesamanagementsystem/AddressesamanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.addressesamanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressesamanagementsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressesamanagementsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/addressesamanagementsystem/AddressesamanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.addressesamanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressesamanagementsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
